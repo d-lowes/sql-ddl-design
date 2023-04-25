8f6f697cf3255606abbab1e1f4e8af4e58f30d1e
Russell Jones <justyrones@gmail.com>
2023-04-25 17:52:59 -0600

    re-did table with references

--- outerspace.sql
+++ outerspace.sql
@@ -1,20 +1,33 @@
 CREATE TABLE planets(
     id TEXT PRIMARY KEY,
-    orbital_period_years FLOAT NOT NULL
+    orbital_period_years FLOAT NOT NULL,
+    planet_star NOT NULL VARCHAR(40) REFERENCES stars
 );
 
-INSERT INTO planets(id, orbital_period_years)
-    VALUES('Earth', '1.00'),
-    ('Mars', '1.882'),
-    ('Venus', '0.62'),
-    ('Proxima Centauri b', '0.03'),
-    ('Gliese 876 b', '0.236');
+INSERT INTO planets(id, orbital_period_years, planet_star)
+    VALUES('Earth', '1.00','Sun'),
+    ('Mars', '1.882','Sun'),
+    ('Venus', '0.62','Sun'),
+    ('Proxima Centauri b', '0.03','Proxima Centauri'),
+    ('Gliese 876 b', '0.236','Gliese 876');
 
 CREATE TABLE stars(
-    id TEXT PRIMARY KEY,
-    star_temp_in_kelvin INT NOT NULL,
-)
+    id  VARCHAR(40) PRIMARY KEY,
+    star_temp_in_kelvin INT NOT NULL
+);
+
+INSERT INTO stars(id, star_temp_in_kelvin)
+    VALUES ('Sun', '5800'),
+           ('Proxima Centauri', '3042'),
+           ('Gliese 876', '3192');
+
 
 CREATE TABLE moons(
-    id TEXT PRIMARY KEY,
-)
+    id  VARCHAR(40) PRIMARY KEY
+    moon_planet NOT NULL VARCHAR(40) REFERENCES planets
+);
+
+INSERT INTO moons(id, moon_planet)
+    VALUES ('Moon','Earth'),
+           ('Phobos','Mars'),
+           ('Deimos','Mars');
